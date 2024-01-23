ac3ed1638d8fb213b0c351e1d7e71b785abd2c34
Dinesha <dinesha.addl@gmail.com>
2024-01-23 13:35:20 +0530

    Testing inline css feature. commented out inline css here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: 'Hello world from inline template',
+  templateUrl: './app.component.html',
+  // template: '<h1>Hello world from inline template</h1>',
   styleUrls: ['./app.component.css']
+  // styles : ['h1 {color :red}']
 })
 export class AppComponent {
   title = 'hotel-inventory';
