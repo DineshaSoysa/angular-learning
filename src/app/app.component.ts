dbf75f37ddf39ba881d9f8d6b093f11d9c0ff522
Dinesha <dinesha.addl@gmail.com>
2024-01-23 13:26:20 +0530

    inline html added for reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: 'Hello world from inline template',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
